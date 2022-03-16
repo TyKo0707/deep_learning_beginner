41e2361998dca1e9fea6207f27000159d54f9166
TyKo0707 <super.family.timut@gmail.com>
2022-03-16 23:22:31 +0200

    Choose layers in VGG16 CNN and set their "trainable" argument to True

--- computer_vision/cats_vs_dogs/transfer_learning.py
+++ computer_vision/cats_vs_dogs/transfer_learning.py
@@ -1,18 +1,80 @@
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense
 from keras.applications.vgg16 import VGG16
 from keras.optimizers import adam_v2
-from c_v_d import train_generator, val_generator, test_generator
-from c_v_d import batch_size, nb_test_samples, nb_train_samples, nb_validation_samples
-import numpy as np
+
+# Каталог с данными для обучения
+train_dir = 'train'
+# Каталог с данными для проверки
+val_dir = 'val'
+# Каталог с данными для тестирования
+test_dir = 'test'
+# Размеры изображения
+img_width, img_height = 150, 150
+
+nb_train_samples = 17500
+nb_validation_samples = 3750
+nb_test_samples = 3750
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+batch_size = 16
+
+train_generator = datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+val_generator = datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+test_generator = datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
 
 vgg16_net = VGG16(weights='imagenet',
                   include_top=False,  # Не будет загружено полносвязная часть сети, только свёртка
                   input_shape=(150, 150, 3))
 
 vgg16_net.trainable = False  # Дообучения сети не будет
+trainable = False
+for layer in vgg16_net.layers:
+    if layer.name == 'block5_conv1' or trainable is True:
+        trainable = True
+        layer.trainable = True
+    print(f'Layer: {layer}\nTrainable:{layer.trainable}')
 
-print(vgg16_net.summary())
+nb_train_samples = 17500
+nb_validation_samples = 3750
+nb_test_samples = 3750
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+img_width, img_height = 150, 150
+batch_size = 16
+
+train_generator = datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+val_generator = datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+test_generator = datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
 
 model = Sequential()
 model.add(vgg16_net)
@@ -28,13 +90,13 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 model.fit_generator(train_generator,
-                    steps_per_epoch=nb_train_samples//batch_size,
-                    epochs=5,
+                    steps_per_epoch=nb_train_samples // batch_size,
+                    epochs=8,
                     validation_data=val_generator,
-                    validation_steps=nb_validation_samples//batch_size)
+                    validation_steps=nb_validation_samples // batch_size)
 
 scores = model.evaluate_generator(test_generator, nb_test_samples // batch_size)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+print("Accuracy: %.2f%%" % (scores[1] * 100))
 
 model.save_weights("cats_vs_dogs.h5")
 model_json = model.to_json()
