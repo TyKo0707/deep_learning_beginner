c9e2631a0a41ee0d1359dd6c4f17f4eb2e6f0a4f
TyKo0707 <super.family.timut@gmail.com>
2022-03-19 20:59:38 +0200

    Verification by photo using dlib library

---
+++ computer_vision/verification_by_photo/dlib_verification.py
@@ -0,0 +1,14 @@
+import dlib
+from skimage import io
+from scipy.spatial import distance
+
+sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+face_rec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+detector = dlib.get_frontal_face_detector()
+
+img = io.imread('images_to_recognize/sozykin_passport.jpg')
+
+win1 = dlib.image_window()
+win1.clear_overlay()
+win1.set_image(img)
+
